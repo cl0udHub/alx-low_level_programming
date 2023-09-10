094a5056c1157d4a9084acd10aa729426491a3d0
cl0udHub <someoneandus@gmail.com>
2023-09-10 13:38:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  a C program that prints line to the standatd error
+ * return 1 (Success)
+ */
+
+int main(void)
+{	
+	putf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n". stdout);
+return (1);
+}
